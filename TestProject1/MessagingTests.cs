c3be66d181093cadad5b4d6349a4e05c60ac5cda
allielevin <aldebaron11@yahoo.com>
2022-07-25 11:13:54 -0700

    #7 Fixed according to comments

--- TestProject1/MessagingTests.cs
+++ TestProject1/MessagingTests.cs
@@ -14,11 +14,12 @@ namespace TestProject1
         [Fact]
         public void ThreadAlJoe()
         {
+            var localService = new MessagingService();
             // Arrange
             int count = 3000;
 
             // Act
-            var thread = MsgService.ReadMessage("Al", "Joe");
+            var thread = localService.ReadMessage("Al", "Joe");
             Console.WriteLine("Messages in thread: " + thread.Count);
 
             // Assert
@@ -26,14 +27,13 @@ namespace TestProject1
             Assert.True(count == thread.Count, "The number of messages between Al and Joe are not as expected: " + count);
         }
 
-        //Just to see how Add would work in tests, Assert statement uses Id number
-        //to see if it gets added before or after the database is built
+        //Tests that Id is at least even, like it's supposed to be
         [Fact]
         public void CheckAdd()
         {
             //arrange
-            var pickanewname = new MessagingService();
-            var m = pickanewname.Add("Al", "EJ", "BIRTHDAY", DateTime.UtcNow);
+            var localService = new MessagingService();
+            var m = localService.Add("Al", "EJ", "BIRTHDAY", DateTime.UtcNow);
             int i;
 
             //act
@@ -41,79 +41,65 @@ namespace TestProject1
 
             //assert
             //Assert that Id is even
-            Assert.True(i == 0, "Id was not even, it was: " + m.Id.ToString()); // use Asert.Equal
+            Assert.Equal(0, i); 
         }
 
-        //Checks to see if the first message in the database is what I expect it to be,
-        //as a way to test the database
+        //Checks for expected count of messages and for a message with specified body.
         [Fact]
         public void CheckMessages()
         {
             //arrange
             int count = 1; // expected number of messages
             var body = "Hi, let's test special chars?&$%^ at the same time. This always needs consideration when viewing in an HTML page or URL (get)."; // specified body
-            MsgService = new MessagingService();
-            MsgService.Add("Cat", "Other Cat", "meow", DateTime.UtcNow);
-            MsgService.Add("Jim", "Steve", body, DateTime.UtcNow);
-            MsgService.Add("Al", "Joe", "Random messages are oddly hard to write", DateTime.UtcNow);
-            int m; // remove
-            string msgFound = "no"; // change to boolean. You are using yes/no as a string -- why not use the precise data type: boolean
+            var localService = new MessagingService();
+            localService.Add("Cat", "Other Cat", "meow", DateTime.UtcNow);
+            localService.Add("Jim", "Steve", body, DateTime.UtcNow);
+            localService.Add("Al", "Joe", "Random messages are oddly hard to write", DateTime.UtcNow);
+            bool msgFound = false;
             bool test; // what is the default status of a bool if you don't specify?
+            //I wrote an assert statement for it but it won't accept an unnassigned variable even tho it's boolean
 
             //act
-           var thread = MsgService.ReadMessage("Jim", "Steve");
-            m = thread.Count; // You are only using m once, for consisenstiy's sake remove m, or use m everwhere thread.Count is used. In this case I prefer removing thread.count.
-            //But this only works if I know who the sender and receiver are, which is pretty limited.
-            // For Unit Tests you ALWAYS have a contrived scenario. In arrange you setup a specific use case/business logic. 
-            // here we have arranged one message is sent. Use the variable "body" in line 57 where you have a string representation. This eliminates errors when the message is changed.
-
+           var thread = localService.ReadMessage("Jim", "Steve");
+           int m = thread.Count;
 
             //WAIT- what if I added a function in MessagingServices that searches all of a users messages for
             //ones containing a keyphrase? This wouldn't really solve the above problem, you'd still need to know
             //at least one user involved, but it would add some cool functionality to the messaging service
             // That's called big brother. Complete view. However this might be an admin function we add down the road for monitoring user's activity. 
             // Still big brother, however if users become unruly, we might have to ban/block their account.
+            //No, sorry, I mean like how Messages on the iphone offers a search *your own* messages function. And when it's actually in use,
+            //bc you'd be searching your own messages, the user would always be known.
 
-
-            //Checks Messages for a message with a specified body
-            //Was there a shorter way to do this? I couldn't think of one.
-            for (int i =0; i < thread.Count; i++) {
-                if (thread[i].Body == body) { msgFound = "yes"; }
+            for (int i =0; i < m; i++) {
+                if (thread[i].Body == body) { msgFound = true; }
             }
 
             //assert
             //Asserts that Jim/Steve thread contains expected number of messages
             //Asserts that there is a message with the specified body
-            Assert.True(count == m, "The database message count is not what it should be"); // This is better as Assert.Equal()
-            Assert.True(msgFound == "yes", "There is no message with the body " + body); // Assert.Ture will work best here for type: bool
+            Assert.Equal(count, m); // It wouldn't allow me to include a failure message? It got very angry and very confusing.
+            Assert.True(msgFound, "There is no message with the body " + body);
+            //Assert.False(test, "The default status of an unspecified bool is true");
         }
 
-        //Tests the ability to use ReadMessage by checking to see if ReadMessage
-        //produces the right number of messages
-        // Get descritive with your test function names. this is part of the documentatin, don't be shy on char length.
+        //Checks to see if ReadMessage produces the right number of messages
         [Fact]
         public void CheckReadCountAlEJ()
         {
             //arrange
-            //MsgService = new MessagingService();
+            var localService = new MessagingService();
             int count = 2000; // expected count of messages
                            
             //act
-            var m = MsgService.ReadMessage("Al", "EJ"); // NOTE: this is act (not arrange)
-            int b = m.Count; 
+            var m = localService.ReadMessage("Al", "EJ"); 
+            int b = m.Count; // actual count of messages
 
             //assert
-            //asserts that count of thread equals expectation (count) // NOTE: Good documentation, can you write this into an Assert.Equal statement so the code line has all the documentation included?
-            //IE: logic of the first parameter should be very clear what you are testing. 
-            // second parameter should tell exactly what went wrong if the test failed.
-            // the above two items should tell a documentation story of what's going on here (positive, or failed case).
-            // Let's see how much ingenuity you can pack into one line of code :)
-            Assert.True(b == count, "bad ConvoThread");
+            Assert.Equal(count, b);
         }
         //This fails every time, but none of the others do! And b is a different number every time, ~3900. I have no idea why.
-        // NOTE: YES!! You've found what I wanted you to see here. Continue to use the global variable. 
-
-        //With a local MsgService, this works! I like the local better than global for these tests, it's much easier to keep track of. 
+        // NOTE: YES!! You've found what I wanted you to see here. Continue to use the global variable.
+        //The global variable? The comment at the top sounded like we were switching to a local variable, did I misunderstand
     }
-    //Maybe final test?
 }
